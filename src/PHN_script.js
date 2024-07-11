657d689d4462def979ac68b2a55abd71e016b1a4
Sharan Suri <31404638+Sharan420@users.noreply.github.com>
2024-07-11 18:58:28 +0530

    changed fb db

--- src/PHN_script.js
+++ src/PHN_script.js
@@ -10,8 +10,27 @@ document.getElementById('mobileForm').addEventListener('submit' ,(e) => {
     deployNumber(numbero.phone);
 });
 
-let SHEET_ID = "1YFLDaeTJTR7DdnMBzkvCwK4h4DmmJiGCRFRX4sJPK1Q"
-let CREDS = "credentials.json"
+//HEARTS!
+const body = document.querySelector("body");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "fas fa-heart";
+    heart.style.left = (Math.random() * 100)+"vw";
+    heart.style.animationDuration = (Math.random()*3)+2+"s"
+    body.appendChild(heart);
+}
+setInterval(createHeart,100);
+
+setInterval(function name(params) {
+    var heartArr = document.querySelectorAll(".fa-heart")
+    if (heartArr.length > 200) {
+       heartArr[0].remove()
+    }
+    console.log(heartArr);
+},100)
+
+
 
 // const mobileumber = document.getElementById('mobileNumber').value.trim();
 // console.log('Submitted phone Number:', mobileumber);
